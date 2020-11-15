5c359daf303ed08bf3fb4d2ebc5cd51ba3950e48
liurui <liurui@MacBook-Pro.lan>
2020-11-15 20:53:40 +0800

    go mod test..

--- add.go
+++ add.go
@@ -2,10 +2,10 @@
 * @Author: liurui
 * @Date:   2020-11-15 20:25:36
 * @Last Modified by:   liurui
-* @Last Modified time: 2020-11-15 20:26:08
+* @Last Modified time: 2020-11-15 20:53:08
 */
 package add
 
-func add(a, b int) int {
+func Add(a, b int) int {
 	return a+b
 }
